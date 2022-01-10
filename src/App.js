82ff2588335f14509049b77c24cd54293dc97c7f
nicola-iyl <nicola.tamburini@inyourlife.info>
2022-01-10 17:00:17 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+import { Provider as AuthProvider } from "./context/AuthContext";
+import { Provider as MessageProvider } from "./context/MessageContext";
+
+import PrivateRoute from "./router/PrivateRoute";
+import PublicRoute from "./router/PublicRoute";
 
 function App() {
+  
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <AuthProvider>
+        <MessageProvider>
+          <BrowserRouter>
+            <Routes>
+              
+            </Routes>
+          </BrowserRouter>
+        </MessageProvider>        
+      </AuthProvider>      
     </div>
   );
 }
